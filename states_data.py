cfc847b2ec4df38ef4556460172cbeeb41fb59de
6thfebruary2025 <6thfebruary2025@gmail.com>
2026-06-02 20:26:36 +0530

    Create states_data.py

---
+++ states_data.py
@@ -0,0 +1,258 @@
+# states_data.py
+
+INDIA_STATES = {
+    "Andhra Pradesh": {
+        "coords": [15.9129, 79.7400],
+        "area_sq_km": "13,101",
+        "total_sanctuaries": 13,
+        "key_species": ["Bengal Tiger", "Indian Leopard", "Four-horned Antelope", "Blackbuck"],
+        "search_image": "Blackbuck Andhra Pradesh"
+    },
+    "Arunachal Pradesh": {
+        "coords": [28.2180, 94.7278],
+        "area_sq_km": "10,154",
+        "total_sanctuaries": 13,
+        "key_species": ["Clouded Leopard", "Red Panda", "Hoolock Gibbon", "Mishmi Takin"],
+        "search_image": "Red Panda Arunachal"
+    },
+    "Assam": {
+        "coords": [26.2006, 92.9376],
+        "area_sq_km": "4,124",
+        "total_sanctuaries": 20,
+        "key_species": ["Greater One-horned Rhinoceros", "Asian Elephant", "Royal Bengal Tiger", "Hoolock Gibbon"],
+        "search_image": "One-horned Rhinoceros Assam"
+    },
+    "Bihar": {
+        "coords": [25.0961, 85.3131],
+        "area_sq_km": "3,447",
+        "total_sanctuaries": 13,
+        "key_species": ["Royal Bengal Tiger", "Indian Gaur", "Gharial", "Gangetic Dolphin"],
+        "search_image": "Gangetic Dolphin Bihar"
+    },
+    "Chhattisgarh": {
+        "coords": [21.2787, 81.8661],
+        "area_sq_km": "6,554",
+        "total_sanctuaries": 14,
+        "key_species": ["Wild Water Buffalo", "Hill Myna", "Sloth Bear", "Four-horned Antelope"],
+        "search_image": "Wild Water Buffalo Chhattisgarh"
+    },
+    "Goa": {
+        "coords": [15.2993, 74.1240],
+        "area_sq_km": "755",
+        "total_sanctuaries": 7,
+        "key_species": ["Gaur (Indian Bison)", "Black Panther", "Malabar Giant Squirrel", "Ruby-throated Bulbul"],
+        "search_image": "Gaur Bison Goa"
+    },
+    "Gujarat": {
+        "coords": [22.2587, 71.1924],
+        "area_sq_km": "17,211",
+        "total_sanctuaries": 23,
+        "key_species": ["Asiatic Lion", "Indian Wild Ass", "Chinkara", "Greater Flamingo"],
+        "search_image": "Asiatic Lion Gir"
+    },
+    "Haryana": {
+        "coords": [29.0588, 76.0856],
+        "area_sq_km": "320",
+        "total_sanctuaries": 10,
+        "key_species": ["Blackbuck", "Sarus Crane", "Nilgai", "Chital"],
+        "search_image": "Sarus Crane Haryana"
+    },
+    "Himachal Pradesh": {
+        "coords": [31.1048, 77.1734],
+        "area_sq_km": "8,358",
+        "total_sanctuaries": 28,
+        "key_species": ["Snow Leopard", "Western Tragopan", "Himalayan Ibex", "Musk Deer"],
+        "search_image": "Snow Leopard Himachal"
+    },
+    "Jharkhand": {
+        "coords": [23.6102, 85.2799],
+        "area_sq_km": "2,382",
+        "total_sanctuaries": 12,
+        "key_species": ["Asian Elephant", "Royal Bengal Tiger", "Sloth Bear", "Four-horned Antelope"],
+        "search_image": "Asian Elephant Jharkhand"
+    },
+    "Karnataka": {
+        "coords": [15.3173, 75.7139],
+        "area_sq_km": "10,214",
+        "total_sanctuaries": 35,
+        "key_species": ["Indian Elephant", "Bengal Tiger", "Gaur", "Indian Giant Squirrel"],
+        "search_image": "Indian Elephant Kabini"
+    },
+    "Kerala": {
+        "coords": [10.8505, 76.2711],
+        "area_sq_km": "3,211",
+        "total_sanctuaries": 18,
+        "key_species": ["Nilgiri Tahr", "Lion-tailed Macaque", "Asian Elephant", "Great Hornbill"],
+        "search_image": "Nilgiri Tahr Kerala"
+    },
+    "Madhya Pradesh": {
+        "coords": [22.9734, 78.6569],
+        "area_sq_km": "10,812",
+        "total_sanctuaries": 24,
+        "key_species": ["Royal Bengal Tiger", "Barasingha", "Indian Leopard", "Sloth Bear"],
+        "search_image": "Barasingha Kanha"
+    },
+    "Maharashtra": {
+        "coords": [19.7515, 75.7139],
+        "area_sq_km": "9,642",
+        "total_sanctuaries": 50,
+        "key_species": ["Malabar Giant Squirrel", "Bengal Tiger", "Forest Owlet", "Chinkara"],
+        "search_image": "Malabar Giant Squirrel"
+    },
+    "Manipur": {
+        "coords": [24.6637, 93.9063],
+        "area_sq_km": "342",
+        "total_sanctuaries": 2,
+        "key_species": ["Sangai (Brow-antlered Deer)", "Hoolock Gibbon", "Blyth's Tragopan", "Leopard Cat"],
+        "search_image": "Sangai Deer Manipur"
+    },
+    "Meghalaya": {
+        "coords": [25.4670, 91.3662],
+        "area_sq_km": "502",
+        "total_sanctuaries": 5,
+        "key_species": ["Clouded Leopard", "Hoolock Gibbon", "Asian Elephant", "Slow Loris"],
+        "search_image": "Clouded Leopard Meghalaya"
+    },
+    "Mizoram": {
+        "coords": [23.1645, 92.9376],
+        "area_sq_km": "1,240",
+        "total_sanctuaries": 10,
+        "key_species": ["Hoolock Gibbon", "Sumatran Serow", "Hume's Pheasant", "Clouded Leopard"],
+        "search_image": "Serow Mizoram"
+    },
+    "Nagaland": {
+        "coords": [26.1584, 94.5624],
+        "area_sq_km": "314",
+        "total_sanctuaries": 4,
+        "key_species": ["Blyth's Tragopan", "Hoolock Gibbon", "Amur Falcon", "Wild Boar"],
+        "search_image": "Amur Falcon Nagaland"
+    },
+    "Odisha": {
+        "coords": [20.9517, 85.0985],
+        "area_sq_km": "8,354",
+        "total_sanctuaries": 21,
+        "key_species": ["Olive Ridley Sea Turtle", "Saltwater Crocodile", "Irrawaddy Dolphin", "Bengal Tiger"],
+        "search_image": "Olive Ridley Turtle Odisha"
+    },
+    "Punjab": {
+        "coords": [31.1471, 75.3412],
+        "area_sq_km": "440",
+        "total_sanctuaries": 13,
+        "key_species": ["Blackbuck", "Indus River Dolphin", "Smooth-coated Otter", "Hog Deer"],
+        "search_image": "Indus Dolphin Punjab"
+    },
+    "Rajasthan": {
+        "coords": [27.0238, 74.2179],
+        "area_sq_km": "9,315",
+        "total_sanctuaries": 26,
+        "key_species": ["Great Indian Bustard", "Bengal Tiger", "Caracal", "Desert Fox"],
+        "search_image": "Great Indian Bustard"
+    },
+    "Sikkim": {
+        "coords": [27.5330, 88.5122],
+        "area_sq_km": "2,624",
+        "total_sanctuaries": 8,
+        "key_species": ["Red Panda", "Snow Leopard", "Blood Pheasant", "Himalayan Monal"],
+        "search_image": "Red Panda Sikkim"
+    },
+    "Tamil Nadu": {
+        "coords": [11.1271, 78.6569],
+        "area_sq_km": "7,024",
+        "total_sanctuaries": 31,
+        "key_species": ["Nilgiri Tahr", "Lion-tailed Macaque", "Indian Elephant", "Grizzled Giant Squirrel"],
+        "search_image": "Lion-tailed Macaque"
+    },
+    "Telangana": {
+        "coords": [18.1124, 79.0193],
+        "area_sq_km": "5,692",
+        "total_sanctuaries": 11,
+        "key_species": ["Spotted Deer (Chital)", "Blackbuck", "Bengal Tiger", "Honey Badger"],
+        "search_image": "Spotted Deer Telangana"
+    },
+    "Tripura": {
+        "coords": [23.9408, 91.9882],
+        "area_sq_km": "603",
+        "total_sanctuaries": 4,
+        "key_species": ["Phayre's Leaf Monkey", "Clouded Leopard", "Capped Langur", "Slow Loris"],
+        "search_image": "Phayres Leaf Monkey"
+    },
+    "Uttar Pradesh": {
+        "coords": [26.8467, 80.9462],
+        "area_sq_km": "3,982",
+        "total_sanctuaries": 26,
+        "key_species": ["Swamp Deer", "Barasingha", "Gharial", "Sarus Crane"],
+        "search_image": "Sarus Crane Uttar Pradesh"
+    },
+    "Uttarakhand": {
+        "coords": [30.0668, 79.0193],
+        "area_sq_km": "6,412",
+        "total_sanctuaries": 14,
+        "key_species": ["Snow Leopard", "Musk Deer", "Himalayan Black Bear", "Bengal Tiger"],
+        "search_image": "Snow Leopard Jim Corbett"
+    },
+    "West Bengal": {
+        "coords": [22.9868, 87.8550],
+        "area_sq_km": "3,415",
+        "total_sanctuaries": 16,
+        "key_species": ["Royal Bengal Tiger", "Fishing Cat", "Estuarine Crocodile", "Indian One-horned Rhino"],
+        "search_image": "Royal Bengal Tiger Sundarbans"
+    },
+    
+    # UNION TERRITORIES
+    "Andaman and Nicobar Islands": {
+        "coords": [11.7401, 92.6586],
+        "area_sq_km": "1,620",
+        "total_sanctuaries": 96,
+        "key_species": ["Dugong (Sea Cow)", "Andaman Wild Boar", "Robber Crab", "Saltwater Crocodile"],
+        "search_image": "Dugong Andaman"
+    },
+    "Chandigarh": {
+        "coords": [30.7333, 76.7794],
+        "area_sq_km": "26",
+        "total_sanctuaries": 2,
+        "key_species": ["Sambar Deer", "Indian Grey Mongoose", "Peafowl", "Golden Jackal"],
+        "search_image": "Sambar Deer Sukhna"
+    },
+    "Dadra and Nagar Haveli and Daman and Diu": {
+        "coords": [20.1809, 73.0169],
+        "area_sq_km": "92",
+        "total_sanctuaries": 1,
+        "key_species": ["Rusty-spotted Cat", "Leopard", "Four-horned Antelope", "Fruit Bat"],
+        "search_image": "Rusty spotted cat"
+    },
+    "Delhi": {
+        "coords": [28.7041, 77.1025],
+        "area_sq_km": "27",
+        "total_sanctuaries": 1,
+        "key_species": ["Nilgai", "Golden Jackal", "Black-crowned Night Heron", "Porcupine"],
+        "search_image": "Nilgai Delhi"
+    },
+    "Jammu and Kashmir": {
+        "coords": [33.7782, 76.5762],
+        "area_sq_km": "2,842",
+        "total_sanctuaries": 15,
+        "key_species": ["Hangul (Kashmir Stag)", "Himalayan Black Bear", "Leopard Cat", "Markhor"],
+        "search_image": "Hangul Kashmir Stag"
+    },
+    "Ladakh": {
+        "coords": [34.1526, 77.5771],
+        "area_sq_km": "4,700",
+        "total_sanctuaries": 2,
+        "key_species": ["Snow Leopard", "Tibetan Wild Ass (Kiang)", "Black-necked Crane", "Bharal"],
+        "search_image": "Kiang Tibetan Wild Ass"
+    },
+    "Lakshadweep": {
+        "coords": [10.5667, 72.6417],
+        "area_sq_km": "12",
+        "total_sanctuaries": 1,
+        "key_species": ["Sooty Tern", "Ghost Crab", "Green Sea Turtle", "Butterfly Fish"],
+        "search_image": "Sooty Tern Lakshadweep"
+    },
+    "Puducherry": {
+        "coords": [11.9416, 79.8083],
+        "area_sq_km": "5",
+        "total_sanctuaries": 1,
+        "key_species": ["Little Egret", "Spotted Owlet", "Golden Jackal", "Fan-throated Lizard"],
+        "search_image": "Spotted Owlet Puducherry"
+    }
+}
